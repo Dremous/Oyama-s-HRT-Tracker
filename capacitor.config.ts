22ffc19ab7f3b626bc98c16457a6cb3a9b5bbe7f
Dremous <dremous@example.com>
2026-05-04 00:42:39 +0800

    Fix Android status bar gap

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,13 @@ const config: CapacitorConfig = {
     bundledWebRuntime: false,
     android: {
         allowMixedContent: false,
+        backgroundColor: '#ffffff',
+    },
+    plugins: {
+        SplashScreen: {
+            launchShowDuration: 0,
+            backgroundColor: '#ffffff',
+        },
     },
 };
 

e3c903512ad9f4050b51e7000c6950e821a35cb5
Dremous <dremous@example.com>
2026-05-04 01:15:23 +0800

    Import mobile.css in index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './src/index.css';
+import './src/styles/mobile.css';
 import App from './src/App';
 
 const container = document.getElementById('root');
